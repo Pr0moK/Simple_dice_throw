73ef8a70e8ade171f981b17c2092215bf8b79577
Pr0moK <42723354+Pr0moK@users.noreply.github.com>
2021-08-25 15:08:25 +0200

    Update Dice.py

--- Dice.py
+++ Dice.py
@@ -3,8 +3,8 @@ import random
 numbers = [1, 2, 3, 4, 5, 6]
 
 while True:
-    IA_Dice = random.choice(numbers)
-    print(IA_Dice)
+    AI_Dice = random.choice(numbers)
+    print(AI_Dice)
 
     play_again = input("Want to throw dice again? (Y)es/(N)o ").upper()
     if play_again != "Y":
