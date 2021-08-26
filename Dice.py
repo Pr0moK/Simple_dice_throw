200cb513c30d353e624285b10162e97c093cf993
Pr0moK <pr0mok1234@gmail.com>
2021-08-26 18:25:38 +0200

    Update Dice.py

--- Dice.py
+++ Dice.py
@@ -2,10 +2,17 @@ import random
 
 numbers = [1, 2, 3, 4, 5, 6]
 
-while True:
-    AI_Dice = random.choice(numbers)
-    print(AI_Dice)
 
+class Dice:
+    def roll_dice(self):
+        while True:
+            IA_Dice = random.choice(numbers)
+            return IA_Dice
+
+
+while True:
+    Output = Dice.roll_dice(numbers)
+    print(Output)
     play_again = input("Want to throw dice again? (Y)es/(N)o ").upper()
     if play_again != "Y":
         break
